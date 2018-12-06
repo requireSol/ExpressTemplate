7f070e0693b8bfc0e753c8ab198294949523fd71
requireSol <solaiman@students.uni-marburg.de>
2018-12-06 18:57:26 +0000

    Setup Express App

---
+++ Rase/info.js
@@ -0,0 +1,15 @@
+/Install express-generator global We need global to execute it:
+//npm install -g express-generator
+
+/generate express appp with dep.
+//express Rase --hogan -c less
+
+/Start Application Rase:
+//DEBUG=my-application ./bin/www
+//var debug = require('debug')('rase:server');
+
+/NodeMon can Monitor your app and restart if you change folderstruct:
+//npm install -g nodemon
+
+/Start nodemon
+//nodemon bin/www
