9ce56e8e6c93548152a548d6d1355daad95ff7a4
requireSol <solaiman@students.uni-marburg.de>
2018-12-06 19:00:42 +0000

    Comment out the FrameWork

--- Rase/app.js
+++ Rase/app.js
@@ -1,3 +1,4 @@
+//Require Modules
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -5,15 +6,20 @@ var cookieParser = require('cookie-parser');
 var lessMiddleware = require('less-middleware');
 var logger = require('morgan');
 
+//Routes Create Route but dont use them Only Path
+//The route we getting from indes.js will be Written to indexRouter
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+//Create App
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+//set view engine to hoganjs
 app.set('view engine', 'hjs');
 
+//Lode Debugger and public stuff
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,6 +27,7 @@ app.use(cookieParser());
 app.use(lessMiddleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Use Route and tell the Paths on Url // Tell were the route starts *1
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
--- Rase/routes/index.js
+++ Rase/routes/index.js
@@ -1,9 +1,23 @@
+//Get Express and Routing engine
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+//ROUTES LIKE /user/:id ///you can get your :id go req.params or req.params.id Here it goes on
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+    //res.send('You can Send everthing');
+    // LIKE 404 401 errors http status and stuff like jsonfiles
+
+    // you can get Post and get Variables from
+    //req.query.name req.query.password req.query.title
+
+
+
+    //Send to routes/index the Title: 'Express' can add , 404 or 200 prob
+    res.render('index', { title: 'Express' });
+
 });
 
+//Start Router File (Return File)
 module.exports = router;
